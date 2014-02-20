23523aaacb747df6c2bc93e361c66a034e2309b3
iva <ilynva@gmail.com>
2014-02-21 01:44:10 +0800

    add coments function pikaia

--- src/pikaiaGAs.jl
+++ src/pikaiaGAs.jl
@@ -6,6 +6,11 @@ importall Base
 export pikaia
 
 function pikaia(ff, n, ctrl, xb, fb, status)
+# Optimization (maximization) of user-supplied "fitness" function
+# ff  over n-dimensional parameter space  x  using a basic genetic
+# algorithm method.
+
+# Version 0.0.1   [ 2014 February 21 ]
 
 end
 
