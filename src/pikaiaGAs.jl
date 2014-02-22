1a6da3cd4b0dfdb2b5b0b9c918edd44e5a831f64
iva <ilynva@gmail.com>
2014-02-22 19:17:15 +0800

    add common block

--- src/pikaiaGAs.jl
+++ src/pikaiaGAs.jl
@@ -5,6 +5,9 @@ importall Base
 
 export pikaia
 
+#  Common block to make iseed visible to rninit (and to save
+iseed=11
+
 function pikaia(ff, n, ctrl, xb, fb, status)
 # Optimization (maximization) of user-supplied "fitness" function
 # ff  over n-dimensional parameter space  x  using a basic genetic
