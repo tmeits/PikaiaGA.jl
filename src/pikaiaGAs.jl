8e9295d712eb3ee9336ffdb7b7b4b152de40f43d
Viktor <vilyin@vilyin.(none)>
2014-02-24 13:13:23 +0800

    add function twod

--- src/pikaiaGAs.jl
+++ src/pikaiaGAs.jl
@@ -3,21 +3,25 @@ module pikaiaGAs
 
 importall Base
 
-export pikaia, seed
+export pikaia
 
-#  Common block to make iseed visible to rninit (and to save
-type seed
-    iseed::Int
-end
-
-function pikaia(ff, n, ctrl, xb, fb, status)
+function pikaia(ff:: Function, n:: Int, ctrl:: Array{Float64, 1})
 # Optimization (maximization) of user-supplied "fitness" function
 # ff  over n-dimensional parameter space  x  using a basic genetic
 # algorithm method.
 
 # Version 0.0.1   [ 2014 February 21 ]
 
-end
+# Init output:
+x = Array{Float64, n}
+f :: Float64 = 0.
+status :: Int = 0
 
-end
+x = rand(n)
+print(ff(n, x))
+# Return in typle
+return (x, f, status)
+end # pikaia
+
+end # pikaiaGAs
 #
