f03164ea9d3ec9f64be97c116e250e7534d2cd9f
iva <ilynva@gmail.com>
2014-03-01 16:42:51 +0800

    Function twod is postponed upward

--- src/main.jl
+++ src/main.jl
@@ -1,6 +1,19 @@
-# Sample driver program for pikaiaGAs.jl
+# Sample driver program for Pikaia.jl
 
-import pikaiaGAs
+function twod(n:: Int, x:: Array{Float64, 1})
+# Compute sample fitness function (2-d landscape)
+	const pi= 3.1415926536
+	const sigma2= 0.15
+	const nn= 9
+
+	if x[1] > 1. || x[2] > 1.
+		println("stop")
+	end
+	rr=sqrt( (0.5 - x[1])^2+ (0.5-x[2])^2) 
+	return cos(rr*nn*pi)^2 *exp(-rr^2/sigma2) 
+end
+
+import Pikaia
 
 # Initialize some empty vectors
 ctrl = Float64[]
@@ -16,20 +29,9 @@ end
 ctrl[2]=50.
 
 # Now call pikaia
-res=pikaiaGAs.pikaia(twod, 2, ctrl)
+res=Pikaia.pikaia(twod, 2, ctrl)
 
 # Print the results
 println(" status: ", res[3])
 
-function twod(n:: Int, x:: Array{Float64, 1})
-# Compute sample fitness function (2-d landscape)
-	const pi= 3.1415926536
-	const sigma2= 0.15
-	const nn= 9
 
-	if x[1] > 1. || x[2] > 1.
-		println("stop")
-	end
-	rr=sqrt( (0.5 - x[1])^2+ (0.5-x[2])^2) 
-	return cos(rr*nn*pi)^2 *exp(-rr^2/sigma2) 
-end
